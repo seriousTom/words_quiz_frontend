c0a651547434cf0ab9b54ea97bf4b8a70f97aaf5
seriousTom <cerniauskas.tomas68@gmail.com>
2026-01-05 03:34:30 +0200

    button style

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,33 +1,66 @@
-import {layout} from "../../styles/layout";
-import {Pressable, Text, View} from "react-native";
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { layout } from "../../styles/layout";
+import { Pressable, Text, View, Animated } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useRef, useState } from "react";
 
-function Button({children, onPress, style, styles = [], options = {}}) {
+function Button({ children, onPress, style, styles = [], options = {} }) {
+    const scale = useRef(new Animated.Value(1)).current;
+    const [isPressed, setIsPressed] = useState(false);
+
     let pressableClass = null;
     let pressedClass = null;
     let textClass = null;
+
     switch (style) {
-        case 'primary':
+        case "primary":
             pressableClass = layout.buttonPrimary;
-            pressedClass = layout.buttonPrimaryActive;
+            pressedClass = layout.buttonPrimaryActive; // for color feedback
             textClass = layout.buttonPrimaryText;
             break;
-        case 'default':
+        case "default":
             pressableClass = layout.buttonDefault;
-            pressedClass = layout.buttonDefaultActive;
+            pressedClass = layout.buttonDefaultActive; // for color feedback
             textClass = layout.buttonDefaultText;
             break;
     }
 
-    return <Pressable
-        style={({pressed}) => {
-            return [layout.button, pressableClass, pressed && pressedClass, pressed && layout.buttonActive, ...styles];
-        }}
-        onPress={onPress}
-    >
-        <Text style={[layout.buttonText, textClass]}>{children}</Text>
-        {options?.icon && <View style={layout.buttonIcon}><Ionicons name={options.icon} size={24} color="white" /></View>}
-    </Pressable>;
+    const onPressIn = () => {
+        Animated.spring(scale, {
+            toValue: 0.96,
+            useNativeDriver: true,
+        }).start();
+        setIsPressed(true);
+    };
+
+    const onPressOut = () => {
+        Animated.spring(scale, {
+            toValue: 1,
+            useNativeDriver: true,
+        }).start();
+        setIsPressed(false);
+    };
+
+    return (
+        <Pressable onPress={onPress} onPressIn={onPressIn} onPressOut={onPressOut}>
+            <Animated.View
+                style={[
+                    layout.button,
+                    pressableClass,
+                    isPressed && pressedClass,
+                    ...styles,
+                    { transform: [{ scale }] },
+                ]}
+            >
+                <Text style={[layout.buttonText, textClass]}>{children}</Text>
+
+                {options?.icon && (
+                    <View style={layout.buttonIcon}>
+                        <Ionicons name={options.icon} size={24} color="white" />
+                    </View>
+                )}
+            </Animated.View>
+        </Pressable>
+    );
 }
 
 export default Button;
